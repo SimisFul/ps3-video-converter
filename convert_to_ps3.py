630fbd69cf7792d878cea352a8cbda80ec2939e1
SimisFul <simon.ps2@sympatico.ca>
2021-10-19 07:18:51 -0400

    refactor: Use recursion to fetch videos from all sub folders given

--- convert_to_ps3.py
+++ convert_to_ps3.py
@@ -2,43 +2,49 @@ import sys, time, os
 
 CRT_RESOLUTION = True
 
-input_paths = sys.argv
+input_paths = sys.argv[1:]
 vid_filenames = []
 
-for it in range(1, len(input_paths)):
-	input_path = input_paths[it]
-
-	if os.path.isdir(input_path):
-		vid_directories = os.listdir(input_path)
-
-		for directory in vid_directories:
-			temp_path = os.path.join(input_path, directory)
-			if os.path.isdir(temp_path):
-				for file in os.listdir(temp_path):
-					if not os.path.isdir(file):
-						vid_filenames.append(os.path.join(temp_path, file))
-			else:
-				vid_filenames.append(temp_path)
-
-	else:
-		vid_filenames.append(input_path)
+if len(input_paths) == 0:
+	exit()
 
 
-	converted_dir = os.path.join(os.path.dirname(__file__), 'converted')
-	if not os.path.exists(converted_dir):
-		os.makedirs(converted_dir)
+def add_files_from_paths(paths):
+    for input_path in paths:
+        paths_list = []
+        # print(os.path.isdir(input_path))
+        # print(paths)
+        if os.path.isdir(input_path):
+            for path in os.listdir(input_path):
+                paths_list.append(os.path.join(input_path, path))
+                # print(os.path.join(input_path, path))
+            add_files_from_paths(paths_list)
+        else:
+            vid_filenames.append(input_path)
+
+
+add_files_from_paths(input_paths)
+
+
+converted_dir = os.path.join(os.path.dirname(__file__), 'converted')
+if not os.path.exists(converted_dir):
+    os.makedirs(converted_dir)
 
 vid_iterator = 1
 
 for vid_to_convert in vid_filenames:
-	filename = os.path.basename(vid_to_convert)
-	print('CURRENT: ' + filename)
-	new_filename = os.path.splitext(filename)[0] + '.mp4'
+    filename = os.path.basename(vid_to_convert)
+    print('CURRENT: ' + filename)
+    new_filename = os.path.splitext(filename)[0] + '.mp4'
 
-	print(str(vid_iterator) + '/' + str(len(vid_filenames)))
-	if CRT_RESOLUTION:
-		os.system('ffmpeg -hide_banner -loglevel error -i \"' + vid_to_convert + '\" -vf scale=-2:\'min(480,ih)\' -c:v h264 -acodec ac3 \"' + os.path.join(converted_dir, new_filename) + '\"')
-	else:
-		os.system('ffmpeg -hide_banner -loglevel error -i \"' + vid_to_convert + '\" -c:v h264 -acodec ac3 \"' + os.path.join(converted_dir, new_filename) + '\"')
-	
-	vid_iterator += 1
+    print(str(vid_iterator) + '/' + str(len(vid_filenames)))
+    if CRT_RESOLUTION:
+        os.system(
+            'ffmpeg -hide_banner -loglevel error -i \"' + vid_to_convert + '\" -vf scale=-2:\'min(480,ih)\' -c:v h264 -acodec ac3 \"' + os.path.join(
+                converted_dir, new_filename) + '\"')
+    else:
+        os.system(
+            'ffmpeg -hide_banner -loglevel error -i \"' + vid_to_convert + '\" -c:v h264 -acodec ac3 \"' + os.path.join(
+                converted_dir, new_filename) + '\"')
+
+    vid_iterator += 1
