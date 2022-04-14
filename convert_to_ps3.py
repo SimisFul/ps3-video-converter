b4c79e49f5222ca0eeeb50a992af9f66098c0332
SimisFul <simon.ps2@sympatico.ca>
2022-04-14 18:38:38 -0400

    fix: Make script actually convert all video formats (thanks mirobarsa.com)

--- convert_to_ps3.py
+++ convert_to_ps3.py
@@ -39,12 +39,19 @@ for vid_to_convert in vid_filenames:
 
     print(str(vid_iterator) + '/' + str(len(vid_filenames)))
     if CRT_RESOLUTION:
-        os.system(
-            'ffmpeg -hide_banner -loglevel error -i \"' + vid_to_convert + '\" -vf scale=-2:\'min(480,ih)\' -c:v h264 -acodec ac3 \"' + os.path.join(
-                converted_dir, new_filename) + '\"')
+        # Old
+        # ffmpeg -hide_banner -loglevel error -i "test.mkv" -vf scale=-2:'min(480,ih)' -c:v h264 -acodec ac3 "converted\test.mp4"
+        # New with subtitles
+        # ffmpeg -hide_banner -loglevel error -i "breakingbad.mkv" -vf "scale=-2:'min(480,ih)', subtitles='breakingbad.mkv':si=0" -c:v libx264 -pix_fmt nv12 -acodec aac -b:a 192k -ac 2 -ar 44100 -af "aresample=async=1:min_hard_comp=0.100000:first_pts=0" -f mp4 "converted\breakingbad_subs.mp4"
+        
+        # Normal convert
+        os.system('ffmpeg -hide_banner -loglevel error -i \"' + vid_to_convert + '\" -vf scale=-2:\'min(480,ih)\' -c:v libx264 -pix_fmt nv12 -acodec aac -b:a 192k -ac 2 -ar 44100 -af \"aresample=async=1:min_hard_comp=0.100000:first_pts=0\" -f mp4 \"' + os.path.join(converted_dir, new_filename) + '\"')
+        
+        # Burn first subtitle track in video (subtitles must be in video file)
+        # command = 'ffmpeg -hide_banner -loglevel error -i \"' + vid_to_convert + '\" -vf \"scale=-2:\'min(480,ih)\', subtitles=\'' + vid_to_convert + '\':si=0\" -c:v libx264 -pix_fmt nv12 -acodec aac -b:a 192k -ac 2 -ar 44100 -af \"aresample=async=1:min_hard_comp=0.100000:first_pts=0\" -f mp4 \"' + os.path.join(converted_dir, new_filename) + '\"'
+        #os.system(command)
     else:
-        os.system(
-            'ffmpeg -hide_banner -loglevel error -i \"' + vid_to_convert + '\" -c:v h264 -acodec ac3 \"' + os.path.join(
-                converted_dir, new_filename) + '\"')
+        os.system('ffmpeg -hide_banner -loglevel error -i \"' + vid_to_convert + '\" -c:v libx264 -pix_fmt nv12 -acodec aac -b:a 192k -ac 2 -ar 44100 -af \"aresample=async=1:min_hard_comp=0.100000:first_pts=0\" -f mp4 \"' + os.path.join(converted_dir, new_filename) + '\"')
+    
 
     vid_iterator += 1
